c7e64a420f42ea5a3f7e0de0f4d86dbf0bfacbad
Ciara Bujanos <c.buj22@gmail.com>
2017-03-22 16:31:16 -0600

    add scoring feature for phase 4

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ var guessBtn = document.querySelector('.guess-btn');
 var clearBtn = document.querySelector('.clear-btn');
 var displayNum = document.querySelector('.display-guess');
 var resetGame = document.querySelector('.reset-btn');
+var score = document.querySelector('.score');
 var min = 0;
 var max = 100;
+var guesses = 0;
+var playerScore = 0;
 
 function randomNum() {
   var newRandom = Math.floor(Math.random() * (max - min)) + min;
@@ -16,29 +19,42 @@ var randomNumber = randomNum();
 console.log(randomNumber);
 
 userInput.addEventListener('keyup', function() {
-  if(event.keyCode == 13) {
+  if (event.keyCode == 13) {
     guessBtn.click();
   }
 })
 
+function scoring() {
+  if (guesses >=1 && guesses <= 3) {
+    playerScore = playerScore + 10;
+    console.log('score', playerScore);
+  } else if (guesses >= 4 && guesses <=7) {
+    playerScore = playerScore + 5;
+  } else if (guesses >= 8 && guesses <= 10){
+    playerScore = playerScore + 2;
+  } else {
+    playerScore = playerScore;
+  }
+}
+
 guessBtn.addEventListener('click', function() {
   var feedback = document.querySelector('.feedback');
-
+  guesses++;
   displayNum.innerText = parseInt(userInput.value);
-  if(isNaN(parseInt(userInput.value))) {
+  if (isNaN(parseInt(userInput.value))) {
     alert("Must enter a number");
-    displayNum.innerText = "?"
+    displayNum.innerText = "?";
   }
   if (userInput.value > max){
     alert("Guess between the range below");
   } else if (userInput.value < min) {
     alert ("Guess between the range below");
   } else if (userInput.value < randomNumber) {
-    feedback.innerText = "That guess was too low, try again!"
+    feedback.innerText = "That guess was too low, try again!";
   } else if (userInput.value > randomNumber) {
-    feedback.innerText = "That guess was too high, maybe next time!"
-  } else  {
-    feedback.innerText = "Boom!! YOU WIN!"
+    feedback.innerText = "That guess was too high, maybe next time!";
+  } else {
+    feedback.innerText = "Boom!! YOU WIN!";
     min = min - 10;
     max = max + 10;
     console.log(min, max);
@@ -46,12 +62,14 @@ guessBtn.addEventListener('click', function() {
     console.log(randomNumber);
     document.querySelector('.min-view').value = min;
     document.querySelector('.max-view').value = max;
+    scoring();
+    score.innerText = playerScore;
   }
 })
 
 userInput.addEventListener('keyup', function() {
-  if(userInput.value === "") {
-    disableBtns()
+  if (userInput.value === "") {
+    disableBtns();
   } else {
     clearBtn.disabled = false;
     resetGame.disabled = false;
@@ -68,8 +86,8 @@ document.querySelector('.max-view', '.min-view').addEventListener('blur', functi
 })
 
 function clearIt() {
-  userInput.value = ""
-  displayNum.innerText = "?"
+  userInput.value = "";
+  displayNum.innerText = "?";
 }
 
 function disableBtns() {
@@ -83,5 +101,5 @@ clearBtn.addEventListener('click', function(){
 })
 
 resetGame.addEventListener('click', function() {
-  location.reload()
+  location.reload();
 })
